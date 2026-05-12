a164c267a488a0ffb80544163953a0dfd192e311
Gabriela Poerari <gabrielapoeraribaptista@gmail.com>
2026-05-12 10:29:06 -0300

    Novo commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = Document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#mesage");
